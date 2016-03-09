27a36a5472a943dc9f53cf2ab425a13d73e18bd3
Vaskill <vasilauskase@gmail.com>
2016-03-09 12:21:50 -0500

    added start button panel

--- Main.java
+++ Main.java
@@ -1,14 +1,27 @@
-import java.awt.Dimension;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
+
 public class Main extends JFrame{
+    
+    private JPanel startScreen;
+    private JButton startButton;
 
 	private Gui gui;
 	public Main(Gui gui){
+            startScreen = new JPanel();                    
+            startButton = new JButton("Start");
+            startScreen.setVisible(true);                   
+            startScreen.add(startButton);
+            ButtonListener bl = new ButtonListener();
+            add(startScreen);
+            
 		this.gui = gui;
 		setSize(400,600);
 		JPanel mainPanel = new JPanel();
@@ -19,10 +32,23 @@ public class Main extends JFrame{
 		mainPanel.add(scrollPane);
 		mainPanel.setVisible(true);
 		getContentPane().add(mainPanel);
-		//pack();
-		setVisible(true);
-		run();
+             setVisible(true);
+          
+            
 	}
+        
+        public class ButtonListener implements ActionListener { 
+            @Override
+            public void actionPerformed(ActionEvent e) { 
+               if(e.getSource()==startButton)
+                {
+                    startScreen.setVisible(false);
+                    
+                    run();
+                }
+            }
+            
+        }
 	  public void run() {
 	    	int ind = 0;
 	        while(!gui.race.getOver()){
